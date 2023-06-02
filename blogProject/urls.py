8696a4d39e11d826a84d9049abb1aa5bdd71ed99
mutemip <paulmutemi2030@gmail.com>
2023-06-02 21:10:44 +0300

    accounts: authentication logic

--- blogProject/urls.py
+++ blogProject/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # blogApp
-    path('', include("blogApp.urls"))
+    path('', include("blogApp.urls")),
+    path('', include("accounts.urls"))
 
 ]
